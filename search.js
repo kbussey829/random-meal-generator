10de56814e6b68cfae8f2fd1c1c2ef3c740a9bb4
kebab <kbussey829@gmail.com>
2026-09-05 16:24:03 -0300

    added cards to search page

---
+++ search.js
@@ -0,0 +1,24 @@
+// define dictionary container
+const cardholder = document.getElementById("cards");
+console.log("linked")
+
+fetch("recipes.json")
+    .then(response => response.json())
+    .then(terms => {
+        // loop through each recipe element
+        terms.forEach(entry => {
+            // start loop
+            const newCard = document.createElement("div");
+
+            // setting the text content (recipe name and ingredients) - from db
+            const titleText = document.createElement("h4");
+            titleText.textContent = entry.name + ": ";
+            const ingredients = document.createElement("p");
+            ingredients.textContent = entry.include; // change this from include to ingredients later
+            newCard.appendChild(titleText);
+            newCard.appendChild(ingredients);
+
+            cardholder.appendChild(newTerm);
+            console.log("term appended")
+        });
+    });
